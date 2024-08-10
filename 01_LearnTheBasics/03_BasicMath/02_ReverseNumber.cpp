ed07b7ae0c4f7d79219d6c6e343d4896f785d517
The-EleetCoder <jaijain1803@gmail.com>
2024-08-10 10:21:00 +0530

    day 3

--- 01_LearnTheBasics/03_BasicMath/02_ReverseNumber.cpp
+++ 01_LearnTheBasics/03_BasicMath/02_ReverseNumber.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/reverse-integer/
 */
+class Solution {
+public:
+    int reverse(int x) {
+        int num = abs(x);
+        int ans = 0;
+        
+        while (num > 0) {
+            int lastDigit = num % 10;  // Extract the last digit of num.
+            
+            // Check if multiplying ans by 10 would stay within 32-bit integer bounds.
+            if (ans >= INT_MIN / 10 && ans <= INT_MAX / 10) {
+                ans = 10 * ans + lastDigit;
+            } else {
+                return 0;
+            }
+            
+            num = num / 10;  // Remove the last digit from num.
+        }
+        
+        // If the original number was negative, negate the result.
+        if (x < 0) return ans * -1;
+        return ans;
+    }
+};
