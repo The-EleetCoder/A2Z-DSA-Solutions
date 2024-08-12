a325c9fd48f7b500d5755b0a171d9dcdcdb1e14e
The-EleetCoder <jaijain1803@gmail.com>
2024-08-12 22:41:34 +0530

    day 4

---
+++ 01_LearnTheBasics/04_BasicRecursion/08_FibonacciNumber.cpp
@@ -0,0 +1,13 @@
+/*
+https://leetcode.com/problems/fibonacci-number/description/
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+
+        return fib(n-1)+fib(n-2);
+    }
+};
