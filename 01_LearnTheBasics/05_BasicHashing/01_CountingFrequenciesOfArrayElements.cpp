d127daa30f5ba2ddc76e071c8993a45d656d2836
The-EleetCoder <jaijain1803@gmail.com>
2024-08-15 22:28:11 +0530

    day 7

--- 01_LearnTheBasics/05_BasicHashing/01_CountingFrequenciesOfArrayElements.cpp
+++ 01_LearnTheBasics/05_BasicHashing/01_CountingFrequenciesOfArrayElements.cpp
@@ -2,13 +2,13 @@
 https://www.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/0
 */
 
-// Approach 1 - using map ( TC: O(nlogn))
+// Approach 1 (using unordered map - TC - O(N) & SC - O(N) )
 class Solution{
     public:
     //Function to count the frequency of all elements from 1 to N in the array.
     void frequencyCount(vector<int>& arr, int N, int P)
     { 
-        map<int, int> mpp;
+        unordered_map<int, int> mpp;
         
         // Counting frequencies of elements in the range [1, N]
         for (int i = 0; i < arr.size(); i++) {
@@ -24,4 +24,43 @@ class Solution{
     }
 };
 
-// Approach 2
+// Approach 2 ( modifying the original array TC - O(N) & SC - O(1))
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr, int N, int P) {
+        int i = 0;
+        while(i<N){
+            // if element is greater than N
+            if (arr[i]>N) {
+                arr[i]=0;
+                i++;
+                continue;
+            }
+            
+            // if element is negative
+            if (arr[i]<=0) {
+                i++;
+                continue;
+            }
+
+            int elementIndex = arr[i]-1; 
+            // if other element is > 0 
+            if (arr[elementIndex] > 0){
+                arr[i] = arr[elementIndex];
+                arr[elementIndex] = -1;
+            }
+            // if other element is <= 0
+            else {
+                arr[elementIndex] -= 1;
+                arr[i] = 0;
+                i++;
+            }
+        }
+        
+        for (int i=0; i<N;i++){
+            if(arr[i] < 0) arr[i] = -arr[i];
+            else arr[i]=0;
+        }
+    }
+};
