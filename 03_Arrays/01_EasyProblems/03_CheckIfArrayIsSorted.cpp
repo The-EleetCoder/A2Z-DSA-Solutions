0c06d9a6263ab772cf4de586f6e34fa243a1eef3
The-EleetCoder <jaijain1803@gmail.com>
2024-08-19 00:48:11 +0530

    day 11

--- 03_Arrays/01_EasyProblems/03_CheckIfArrayIsSorted.cpp
+++ 03_Arrays/01_EasyProblems/03_CheckIfArrayIsSorted.cpp
@@ -2,3 +2,27 @@
 https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
 */
 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Edge case: If the array has 2 or fewer elements, it's always "sorted".
+        if (n <= 2) return true;
+
+        int count = 0;
+
+        // Count the number of "drops" or places where the array is not in ascending order.
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
+        }
+
+        // Check the boundary between the last and the first element
+        if (nums[n - 1] > nums[0]) count++;
+
+        // If there's more than one "drop", it can't be a valid rotated sorted array.
+        return count <= 1;
+    }
+};
