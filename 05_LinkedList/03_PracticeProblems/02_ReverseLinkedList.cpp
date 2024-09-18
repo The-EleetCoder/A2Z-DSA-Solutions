48043a7178ca4b144aa3c24139be5f18c4c77994
The-EleetCoder <jaijain1803@gmail.com>
2024-09-18 16:12:54 +0530

    day 35

--- 05_LinkedList/03_PracticeProblems/02_ReverseLinkedList.cpp
+++ 05_LinkedList/03_PracticeProblems/02_ReverseLinkedList.cpp
@@ -6,6 +6,7 @@ https://leetcode.com/problems/reverse-linked-list/
 The `reverseList` function reverses a singly linked list using an iterative approach. It initializes three pointers: `prev` (set to `NULL`), `curr` (starting at the head of the list), and `nextt` (also initialized to the head). During each iteration, the function stores the next node, reverses the link by making the current node point to the previous one, then updates `prev` to the current node and moves `curr` to the next node. This process continues until all nodes are reversed. The function then returns `prev`, which now points to the new head of the reversed list. The approach operates in O(n) time with O(1) space.
 */
 
+// iterative way
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -20,5 +21,24 @@ public:
             curr = nextt;
         }
         return prev;
+    }
+};
+
+// recursive way
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        // base case
+        if (head == NULL || head->next==NULL)
+            return head;
+
+        ListNode* newHead = reverseList(head->next);
+
+        ListNode* temp = head->next;
+        head->next = NULL;
+        temp->next = head;
+
+        return newHead;
     }
 };
